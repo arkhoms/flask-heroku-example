c2800956806d8ef60360715ae852e8deaa2b6aec
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-12 10:06:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     """Return homepage."""
 #    json_data = {'Hello': 'World!'}
 #    return jsonify(json_data)
-    return "ชื่อเรา"
+    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ห้อง 4/0"
+
 if __name__ == '__main__':
     app.run()
